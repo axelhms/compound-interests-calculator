c6a8e6de4c11c07b54f79e7f31575aa088dc3870
Axel <axel.hemissi@agena3000.com>
2021-10-10 19:04:31 +0200

    Form

---
+++ components/Wrapper.tsx
@@ -0,0 +1,34 @@
+import { Box, Flex, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+
+export type WrapperVariant = 'small' | 'regular';
+
+interface WrapperProps {
+	variant?: WrapperVariant;
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({
+	children,
+	variant = 'regular',
+}) => {
+	const { colorMode } = useColorMode();
+
+	return (
+		<Flex
+			minHeight="100vh"
+			bgColor={colorMode === 'light' ? 'white' : 'dark'}
+			color={colorMode === 'light' ? 'dark' : 'white'}
+		>
+			<Box
+				pt={8}
+				mx="auto"
+				maxWidth={variant === 'regular' ? '1200px' : '800px'}
+				w="100%"
+			>
+				{children}
+			</Box>
+		</Flex>
+	);
+};
+
+export default Wrapper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,63 @@
+import { Box, Flex, Heading } from '@chakra-ui/layout';
+import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
 import type { NextPage } from 'next';
+import React from 'react';
+import InputField from '../components/InputField';
+import Wrapper from '../components/Wrapper';
+
+interface Values {
+	initialInvestment: number;
+	interestRate: number;
+	term: number;
+}
 
 const Home: NextPage = () => {
-	return <div>Hello</div>;
+	const initialValues: Values = {
+		initialInvestment: 0,
+		interestRate: 0,
+		term: 0,
+	};
+
+	return (
+		<Wrapper variant="regular">
+			<Heading>Calculator</Heading>
+			<Formik
+				initialValues={initialValues}
+				onSubmit={(values, actions) => {
+					console.log({ values, actions });
+					alert(JSON.stringify(values, null, 2));
+					actions.setSubmitting(false);
+				}}
+			>
+				<Form>
+					<Box mt={4}>
+						<InputField
+							name="initialInvestment"
+							label="Initial investment"
+						/>
+					</Box>
+
+					<Box mt={4}>
+						<InputField
+							name="interestRate"
+							label="Interest rate (%)"
+						/>
+					</Box>
+
+					<Box mt={4}>
+						<InputField name="term" label="Term (in years)" />
+					</Box>
+
+					<Flex>
+						<Button mt={4} ml="auto" type="submit">
+							Submit
+						</Button>
+					</Flex>
+				</Form>
+			</Formik>
+		</Wrapper>
+	);
 };
 
 export default Home;
